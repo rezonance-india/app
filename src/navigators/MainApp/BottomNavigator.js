c78b5204d7271e1770c750e3685fc4b431b89089
kg-kartik <goel.kartik39@gmail.com>
2021-03-01 14:26:57 +0530

    Added bottom navigation

---
+++ src/navigators/MainApp/BottomNavigator.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import {Dimensions} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {enableScreens} from 'react-native-screens';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+//Colors
+import {GRAY, PRIMARY} from '../../constants/colors';
+import HomeScreen from '../../screens/MainApp/HomeScreen';
+import SearchScreen from '../../screens/MainApp/SearchScreen';
+import ChatScreen from '../../screens/MainApp/ChatScreen';
+
+const Tab = createBottomTabNavigator();
+
+const {width, height} = Dimensions.get('window');
+
+const BottomNavigator = ({navigation}) => {
+  return (
+    <Tab.Navigator
+      initialRouteName="HomeScreen"
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+          if (route.name === 'HomeScreen') {
+            iconName = 'home-outline';
+          } else if (route.name === 'SearchScreen') {
+            iconName = 'search-outline';
+          } else if (route.name === 'ChatScreen') {
+            iconName = 'chatbox-outline';
+          }
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: PRIMARY,
+        inactiveTintColor: GRAY.T6,
+        keyboardHidesTabBar: true,
+        showLabel: false,
+        style: {
+          height: height / 16,
+          borderWidth: 0,
+        },
+      }}>
+      <Tab.Screen name="HomeScreen" component={HomeScreen} />
+      <Tab.Screen name="SearchScreen" component={SearchScreen} />
+      <Tab.Screen name="ChatScreen" component={ChatScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomNavigator;
