0bbf4064afc1dca0136a708eda0b1f7155c5682c
kg-kartik <goel.kartik39@gmail.com>
2021-03-05 14:52:09 +0530

    Added utilities

--- src/screens/MainApp/ChatScreen.js
+++ src/screens/MainApp/ChatScreen.js
@@ -3,7 +3,7 @@ import {Text, View} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 const ChatScreen = () => {
-  return <></>;
+  return <Text>lol</Text>;
 };
 
 export default ChatScreen;
