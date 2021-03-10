d8e47fe3d0494ccca8cf9b0bd80f7c3db40e010a
radioactive11 <roy.arijit2001@gmail.com>
2021-03-10 17:16:33 +0530

    added few dummy tracks

--- src/components/Player/App.js
+++ src/components/Player/App.js
@@ -37,12 +37,28 @@ export default class MusicPlayer extends Component {
 	render() {
 		const TRACKS = [
 			{
-				title: '9.13',
-				artist: 'lol',
+				title: 'Wildest Dreams',
+				artist: 'Taylor Swift',
+				albumArtUrl:
+					'https://i.scdn.co/image/ab67616d0000b273332d85510aba3eb28312cfb2',
+				audioUrl:
+					'http://schnncdnems03.cdnsrv.jio.com/jiosaavn.cdn.jio.com/886/72783d1150205788c0f9413233f8e982_96.mp4',
+			},
+			{
+				title: 'Red',
+				artist: 'Taylor Swift',
+				albumArtUrl:
+					'https://i.scdn.co/image/ab67616d0000b27396384c98ac4f3e7c2440f5b5',
+				audioUrl:
+					'http://snoidcdnems07.cdnsrv.jio.com/jiosaavn.cdn.jio.com/049/8bbc73dac899d6f14052d6fac9d7ee86_160.mp4',
+			},
+			{
+				title: 'Vertigo',
+				artist: 'Khalid',
 				albumArtUrl:
 					'https://i.scdn.co/image/ab67616d0000b273c43edd2cf01edf73bf0b97a3',
 				audioUrl:
-					'http://schnncdnems03.cdnsrv.jio.com/jiosaavn.cdn.jio.com/385/ff28bce85b73d8eb1294630af0df2dc5_96.mp4',
+					'http://schnncdnems03.cdnsrv.jio.com/jiosaavn.cdn.jio.com/385/a6fa35ad1a1313d8d3e412352fe9c51a_96.mp4',
 			},
 			{
 				title: 'Vertigo',
