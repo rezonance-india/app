d5da62c926c8cba98ceb232a74afe6362842ef1e
kg-kartik <goel.kartik39@gmail.com>
2021-03-16 17:23:26 +0530

    Added custom search box

---
+++ src/components/Shared/SearchBox.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {colors} from '../../constants/colors';
+import InputBox from './InputBox';
+
+const SearchBox = ({setSearchQuery, searchQuery, navigation}) => {
+	const handleBack = () => {
+		console.log('hello');
+	};
+
+	const pressIcon = () => {};
+
+	return (
+		<View
+			style={{
+				paddingHorizontal: 20,
+				paddingTop: 30,
+				paddingBottom: 10,
+				backgroundColor: colors.background,
+				flexDirection: 'row',
+			}}>
+			<InputBox
+				value={searchQuery}
+				onChangeText={(value) => setSearchQuery(value)}
+				placeholder="Search song/artists"
+				// label={'search'}
+				style={{
+					borderRadius: 5,
+				}}
+				// handleIconPress={pressIcon}
+				autoFocus={true}
+				icon={'X'}
+				viewStyle={{
+					flex: 1,
+					flexDirection: 'row',
+					borderBottomWidth: 1,
+					paddingBottom: 10,
+				}}
+			/>
+		</View>
+	);
+};
+
+export default SearchBox;
