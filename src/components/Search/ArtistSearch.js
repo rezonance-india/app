6bf915cd80325b9fde036e7104522027917a3e23
kg-kartik <goel.kartik39@gmail.com>
2021-06-07 13:23:14 +0530

    Added screen for artist search

--- src/components/Search/ArtistSearch.js
+++ src/components/Search/ArtistSearch.js
@@ -1,16 +1,116 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useContext, useState} from 'react';
+import {ScrollView, FlatList, Text, View, TextInput, Image} from 'react-native';
+import axios from 'axios';
+import {apiUrl} from '../../constants/config';
+import _ from 'lodash';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import SearchBox from '../../components/Search/SearchBox';
+import LinearGradientComp from '../Shared/LinearGradient';
+import {colors} from '../../constants/colors';
 
-const ArtistSearch = () => {
+const ArtistSearch = ({navigation}) => {
+	const [result, setResult] = useState([]);
+	const [searchQuery, setSearchQuery] = useState('');
+
+	//Renderer function
+	const renderer = ({item}) => {
+		const viewArtist = () => {
+			// navigation.navigate('ViewArtistScreen');
+			console.log('ok');
+		};
+		return (
+			<TouchableOpacity onPress={viewArtist}>
+				<View
+					style={{
+						flexDirection: 'column',
+						marginVertical: 10,
+						marginHorizontal: 20,
+					}}>
+					<Image
+						source={{uri: item.artist_image}}
+						style={{
+							borderRadius: 100,
+							borderColor: 'white',
+							borderWidth: 3,
+							width: 150,
+							height: 150,
+						}}
+					/>
+					<View
+						style={{
+							flexDirection: 'row',
+							justifyContent: 'center',
+						}}>
+						<Text
+							style={{
+								color: 'white',
+								fontFamily: 'IBMPlexSans',
+								marginVertical: 20,
+								fontSize: 18,
+								fontWeight: 'bold',
+								letterSpacing: 0.4,
+							}}>
+							{item.artist_name}
+						</Text>
+					</View>
+				</View>
+			</TouchableOpacity>
+		);
+	};
+
+	const search = _.debounce((value) => {
+		console.log(value, 'value');
+		if (value.length === 0) {
+			console.log('empty');
+			setResult([]);
+		}
+		if (value.length !== 0) {
+			axios
+				.post(
+					`${apiUrl}search/artists`,
+					{
+						query: value,
+					},
+					{
+						headers: {
+							'Content-Type': 'application/json',
+						},
+					},
+				)
+				.then((res) => {
+					setResult(res.data);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		}
+	}, 500);
 	return (
-		<View>
-			<Text
+		<LinearGradientComp
+			bgcolors={{
+				colorOne: colors.search,
+				colorTwo: colors.search,
+			}}>
+			<View
 				style={{
-					color: 'white',
+					marginTop: 20,
 				}}>
-				Artist
-			</Text>
-		</View>
+				<SearchBox
+					placeholder="Search Artists/Songs"
+					searchQuery={searchQuery}
+					setSearchQuery={search}
+					navigation={navigation}
+				/>
+			</View>
+
+			<FlatList
+				keyExtractor={(item) => item.artist_id}
+				data={result}
+				renderItem={renderer}
+				numColumns={2}
+				showsVerticalScrollIndicator={false}
+			/>
+		</LinearGradientComp>
 	);
 };
 
