f24d0337d1d8dee6eaf9bedff8920e6e672c44ef
kg-kartik <goel.kartik39@gmail.com>
2021-06-09 00:28:01 +0530

    Added album search screen

--- src/components/Search/AlbumSearch.js
+++ src/components/Search/AlbumSearch.js
@@ -1,16 +1,178 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useContext, useState} from 'react';
+import {FlatList, View, Image, Dimensions} from 'react-native';
+import axios from 'axios';
+import {apiUrl} from '../../constants/config';
+import _ from 'lodash';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import SearchBox from '../../components/Search/SearchBox';
+import {GlobalContext} from '../../context/GlobalState';
+import LinearGradientComp from '../Shared/LinearGradient';
+import {colors} from '../../constants/colors';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Type from '../Shared/Type';
+import SimpleList from '../Shared/SimpleList';
 
-const AlbumSearch = () => {
-	return (
-		<View>
-			<Text
-				style={{
-					color: 'white',
+const {width, height} = Dimensions.get('screen');
+
+const AlbumSearch = ({navigation}) => {
+	const [result, setResult] = useState([]);
+	const [searchQuery, setSearchQuery] = useState('');
+	const [selectedSong, setSelectedSong] = useState({});
+	const [disable, setDisable] = useState(false);
+	const {queue, updateQueue} = useContext(GlobalContext);
+	const renderer = ({item}) => {
+		return (
+			<TouchableOpacity
+				activeOpacity={0.75}
+				onPress={() => {
+					axios
+						.post(
+							`${apiUrl}fetch/tracks`,
+							{
+								album_id: item.album_id,
+							},
+							{
+								headers: {
+									'Content-Type': 'application/json',
+								},
+							},
+						)
+						.then((res) => {
+							// console.log(res.data, 'data');
+							const result = res.data.map((track) => ({
+								...track,
+								track_img: item.album_img,
+								artist_name: item.artist_name,
+							}));
+							console.log(result, 'data');
+							navigation.navigate('ViewArtistScreen', {
+								tracksData: result,
+								album_image: item.album_img,
+								artist_name: item.artist_name,
+								album_name: item.album_name,
+							});
+						})
+						.catch((err) => {
+							console.log(err);
+						});
 				}}>
-				Album
-			</Text>
-		</View>
+				<View style={{flexDirection: 'row', width: '100%'}}>
+					<View
+						style={{
+							width: width / 7,
+							height: width / 7,
+							borderRadius: 1,
+							marginVertical: 7,
+							marginHorizontal: 10,
+							justifyContent: 'center',
+							alignItems: 'center',
+						}}>
+						<Image
+							source={{
+								uri: item.album_img,
+							}}
+							style={{
+								width: 50,
+								height: 50,
+								borderRadius: 6,
+								overflow: 'hidden',
+							}}
+						/>
+					</View>
+
+					<View
+						style={{
+							marginVertical: 10,
+							marginHorizontal: 15,
+							justifyContent: 'space-around',
+							flex: 1,
+						}}>
+						<View
+							style={{
+								flexDirection: 'row',
+								marginTop: 5,
+								justifyContent: 'space-between',
+								flex: 1,
+								width: '100%',
+							}}>
+							<Type
+								style={{
+									fontSize: width / 24,
+									width: '80%',
+									color: colors.text,
+									fontWeight: 'bold',
+								}}>
+								{item.album_name.length > 30
+									? `${item.album_name.substring(0, 20)}....`
+									: item.album_name}
+							</Type>
+						</View>
+
+						<Type
+							style={{
+								fontSize: width / 26,
+								color: '#D3D3D3',
+							}}>
+							{item.artist_name}
+						</Type>
+					</View>
+				</View>
+			</TouchableOpacity>
+		);
+	};
+
+	const search = _.debounce((value) => {
+		console.log(value, 'value');
+		if (value.length === 0) {
+			console.log('empty');
+			setResult([]);
+		}
+		if (value.length !== 0) {
+			axios
+				.post(
+					`${apiUrl}search/albums`,
+					{
+						query: value,
+					},
+					{
+						headers: {
+							'Content-Type': 'application/json',
+						},
+					},
+				)
+				.then((res) => {
+					setResult(res.data);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		}
+	}, 500);
+	// const searchHeader = () => <Type>Search</Type>;
+	return (
+		<LinearGradientComp
+			bgcolors={{
+				colorOne: colors.search,
+				colorTwo: colors.search,
+			}}>
+			<View
+				style={{
+					marginTop: 20,
+				}}>
+				<SearchBox
+					placeholder="Search Artists/Songs"
+					searchQuery={searchQuery}
+					setSearchQuery={search}
+					navigation={navigation}
+				/>
+			</View>
+			<FlatList
+				keyExtractor={(item) => item.album_id}
+				data={result}
+				renderItem={renderer}
+				showsVerticalScrollIndicator={false}
+			/>
+		</LinearGradientComp>
 	);
 };
 
