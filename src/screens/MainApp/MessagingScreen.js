423748cde2f7678e9c2e77851cf36726ac16c99d
kg-kartik <goel.kartik39@gmail.com>
2021-06-25 12:46:52 +0530

    Minor styling changes

--- src/screens/MainApp/MessagingScreen.js
+++ src/screens/MainApp/MessagingScreen.js
@@ -11,9 +11,10 @@ const {width, height} = Dimensions.get('window');
 const MessagingScreen = ({route,navigation}) => {
     const user = route.params.item;
 
-    // const handleBack = () => {
-    //     navigation.canGoBack();
-    // }
+    const handleBack = () => {
+        // navigation.canGoBack();
+		console.log("lol");
+	}
 
 	const renderer = ({item}) => {
 		return (
@@ -24,10 +25,10 @@ const MessagingScreen = ({route,navigation}) => {
 					imageStyle={{borderRadius: 10}}>
 					<View
 						style={{
-							height: '35%',
-							top: '85%',
+							height: '20%',
+							top: '80%',
 							width: '100%',
-							backgroundColor: '#000',
+							backgroundColor: '#1c1b19',
 							opacity: 0.65,
 							borderBottomLeftRadius: 10,
 							borderBottomRightRadius: 10,
@@ -36,8 +37,8 @@ const MessagingScreen = ({route,navigation}) => {
 					<Text
 						style={{
 							...styles.text,
-							fontSize: 15,
-							fontFamily: 'NotoSans',
+							fontSize: 20,
+							fontFamily: '',
 							fontWeight: 'bold',
 						}}>
 						{item.songDetails.track_name}
@@ -55,38 +56,42 @@ const MessagingScreen = ({route,navigation}) => {
 				colorTwo: ACCENT,
 			}}>
                 
-            <View style={{
-                marginLeft:"10%",
-                marginTop:"10%",
-                flexDirection:"row",
-            }}>
+				<View style={{
+					marginTop:"10%",
+					marginLeft:"10%",
+					marginBottom:20,
+					flexDirection:"row",
+				}}>
 
-                {/* {navigation.canGoBack() ? (
+
 					<TouchableOpacity onPress={handleBack} activeOpacity={0.75}>
 						<Icon
-							name="chevron-back-outlined"
-							size={22}
-							style={{margin: 5, colors:"white"}}
+							name="arrow-back-outline"
+							size={30}
+							style={{marginVertical: 5,marginHorizontal:15, color:"white"}}
 						/>
 					</TouchableOpacity>
-				) : null} */}
 
-                <Image
-                    source={{
-                        uri:user.image
-                    }}
-                    style={{
-                        borderRadius: 20,
-						width: 50,
-						height: 50
-                    }}
-                />
-                <Text style={{
-                    marginLeft:"4%",
-                    color:"white",
-                    marginTop:"2%"
-                }}>{user.name}</Text>
-            </View>            
+					<Image
+						source={{
+							uri:user.image
+						}}
+						style={{
+							borderRadius: 20,
+							width: 50,
+							height: 50
+						}}
+					/>
+					<Text style={{
+						marginLeft:"4%",
+						color:"white",
+						marginTop:"2%",
+						fontFamily:"NotoSans-Bold",
+						fontSize:16
+					}}>{user.name}</Text>
+            	
+				</View>            
+			
 			<FlatList
 				keyExtractor={(item) => item.songDetails.track_id}
 				data={messageData}
@@ -108,10 +113,10 @@ const styles = StyleSheet.create({
 		height: (width * 8) / 10,
 	},
 	text: {
-		fontSize: 12,
-		top: '52%',
+		fontSize: 16,
+		top: '60%',
 		color: 'white',
-		fontFamily: 'NotoSans-Regular',
+		fontFamily: 'IBMPlexSans-Regular',
 		marginHorizontal: 8,
 		marginTop: 2,
 	},
